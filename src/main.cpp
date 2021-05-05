a66b7afd6714b0a2a50adb5dd3a766d7b9660ac5
henwill8 <43818215+henwill8@users.noreply.github.com>
2021-05-05 11:53:13 -0600

    Possibly fix recording being dark

--- src/main.cpp
+++ src/main.cpp
@@ -2120,7 +2120,7 @@ MAKE_HOOK_OFFSETLESS(LightManager_OnWillRenderObject, void, Il2CppObject* self) 
                 
                 cameraGameObject->get_transform()->set_eulerAngles(UnityEngine::Vector3(0.0f, 0.0f, 0.0f));
                 cameraGameObject->get_transform()->set_position(UnityEngine::Vector3(0.0f, 2.0f, 0.0f));
-                texture = UnityEngine::RenderTexture::New_ctor(width, height, 24);
+                texture = UnityEngine::RenderTexture::New_ctor(width, height, 24, UnityEngine::RenderTextureFormat::RGB24, UnityEngine::RenderTextureReadWrite::sRGB);
                 texture->Create();
                 UnityEngine::RenderTexture::set_active(texture);
                 UnityEngine::Object::DontDestroyOnLoad(texture);
