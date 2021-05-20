56cfbfd2adb5e4b3231642dd616e761deccdef35
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-05-19 22:44:21 -0400

    Update libs

--- Android.mk
+++ Android.mk
@@ -44,11 +44,11 @@ LOCAL_MODULE := bs-utils
 LOCAL_EXPORT_C_INCLUDES := extern/bs-utils
 LOCAL_SRC_FILES := extern/libbs-utils.so
 include $(PREBUILT_SHARED_LIBRARY)
-# Creating prebuilt for dependency: beatsaber-hook - version: 1.3.3
+# Creating prebuilt for dependency: beatsaber-hook - version: 1.3.5
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_1_3_3
+LOCAL_MODULE := beatsaber-hook_1_3_5
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_3_3.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_1_3_5.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: codegen - version: 0.8.1
 include $(CLEAR_VARS)
@@ -112,7 +112,7 @@ LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
 LOCAL_SHARED_LIBRARIES += modloader
-LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_3_3
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_1_3_5
 LOCAL_SHARED_LIBRARIES += codegen_0_8_1
 LOCAL_SHARED_LIBRARIES += questui
 LOCAL_SHARED_LIBRARIES += custom-types
--- buildBMBF.ps1
+++ buildBMBF.ps1
@@ -8,7 +8,7 @@ if (-not ($PSVersionTable.PSEdition -eq "Core")) {
 }
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/libreplay.so","./mod.json","./libs/arm64-v8a/libbeatsaber-hook_1_3_3.so","./libs/arm64-v8a/libbs-utils.so","./libs/arm64-v8a/libquestui.so","./ffmpeg/libavcodec.so","./ffmpeg/libavdevice.so","./ffmpeg/libavfilter.so","./ffmpeg/libavutil.so","./ffmpeg/libavformat.so","./ffmpeg/libswresample.so","./ffmpeg/libswscale.so","./ffmpeg/libffmpegkit_abidetect.so","./ffmpeg/libffmpegkit.so","./ffmpeg/libc++_shared.so" -DestinationPath "./$Name.zip" -Update
+Compress-Archive -Path "./libs/arm64-v8a/libreplay.so","./mod.json","./libs/arm64-v8a/libbeatsaber-hook_1_3_5.so","./libs/arm64-v8a/libbs-utils.so","./libs/arm64-v8a/libquestui.so","./ffmpeg/libavcodec.so","./ffmpeg/libavdevice.so","./ffmpeg/libavfilter.so","./ffmpeg/libavutil.so","./ffmpeg/libavformat.so","./ffmpeg/libswresample.so","./ffmpeg/libswscale.so","./ffmpeg/libffmpegkit_abidetect.so","./ffmpeg/libffmpegkit.so","./ffmpeg/libc++_shared.so" -DestinationPath "./$Name.zip" -Update
 $FileName = "./$Name.qmod"
 if(Test-Path $FileName) {
   Remove-Item $FileName
