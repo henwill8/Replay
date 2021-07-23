1b5bd8c157a393b7f96a1ebc55b44ef5ca121e72
Henwill8 <henwill8@yahoo.com>
2021-07-22 22:43:51 -0600

    Added the safety check

--- src/CustomTypes/AsyncGPUReadbackPluginRequest.cpp
+++ src/CustomTypes/AsyncGPUReadbackPluginRequest.cpp
@@ -81,6 +81,13 @@ extern "C" void makeRequest_renderThread(int event_id) {
 	glGetTexLevelParameteriv(GL_TEXTURE_2D, task->miplevel, GL_TEXTURE_INTERNAL_FORMAT, &(task->internal_format));
 	task->size = task->width * task->height * getPixelSizeFromInternalFormat(task->internal_format);
 
+    if (task->size == 0
+		|| getFormatFromInternalFormat(task->internal_format) == 0
+		|| getTypeFromInternalFormat(task->internal_format) == 0) {
+		task->error = true;
+		return;
+	}
+
 	// Create the fbo (frame buffer object) from the given texture
 	glGenFramebuffers(1, &(task->fbo));
 
