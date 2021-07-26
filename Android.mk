0aa26d67fd2ca90cd5942100abca4c6fe459a0b3
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-07-26 18:53:57 -0400

    I want to stay on 1.16.2

--- Android.mk
+++ Android.mk
@@ -20,7 +20,7 @@ TARGET_ARCH_ABI := arm64-v8a
 include $(CLEAR_VARS)
 LOCAL_MODULE := hook
 rwildcard=$(wildcard $1$2) $(foreach d,$(wildcard $1*),$(call rwildcard,$d/,$2))
-# Creating prebuilt for dependency: modloader - version: 1.2.3
+# Creating prebuilt for dependency: modloader - version: 1.1.0
 include $(CLEAR_VARS)
 LOCAL_MODULE := modloader
 LOCAL_EXPORT_C_INCLUDES := extern/modloader
@@ -46,9 +46,9 @@ LOCAL_SRC_FILES := extern/libbs-utils.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: beatsaber-hook - version: 2.2.0
 include $(CLEAR_VARS)
-LOCAL_MODULE := beatsaber-hook_2_2_0
+LOCAL_MODULE := beatsaber-hook_2_0_3
 LOCAL_EXPORT_C_INCLUDES := extern/beatsaber-hook
-LOCAL_SRC_FILES := extern/libbeatsaber-hook_2_2_0.so
+LOCAL_SRC_FILES := extern/libbeatsaber-hook_2_0_3.so
 include $(PREBUILT_SHARED_LIBRARY)
 # Creating prebuilt for dependency: codegen - version: 0.10.2
 include $(CLEAR_VARS)
@@ -116,6 +116,12 @@ LOCAL_MODULE := replay
 LOCAL_SRC_FILES += $(call rwildcard,src/,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.cpp)
 LOCAL_SRC_FILES += $(call rwildcard,extern/beatsaber-hook/src/inline-hook,*.c)
+LOCAL_SHARED_LIBRARIES += modloader
+LOCAL_SHARED_LIBRARIES += codegen_0_10_2
+LOCAL_SHARED_LIBRARIES += beatsaber-hook_2_0_3
+LOCAL_SHARED_LIBRARIES += questui
+LOCAL_SHARED_LIBRARIES += custom-types
+LOCAL_SHARED_LIBRARIES += bs-utils
 LOCAL_SHARED_LIBRARIES += avcodec
 LOCAL_SHARED_LIBRARIES += avformat
 LOCAL_SHARED_LIBRARIES += avutil
@@ -127,12 +133,6 @@ LOCAL_LDLIBS += -llog -lGLESv3 -lGLESv2
 LOCAL_CPPFLAGS += -std=c++2a -O3
 LOCAL_C_INCLUDES += ./include ./shared ./src ./extern ./extern/libil2cpp/il2cpp/libil2cpp ./extern/codegen/include ./ffmpeg
 LOCAL_CPP_FEATURES += exceptions
-LOCAL_STATIC_LIBRARIES += beatsaber-hook_2_2_0
-LOCAL_STATIC_LIBRARIES += modloader
-LOCAL_STATIC_LIBRARIES += bs-utils
-LOCAL_STATIC_LIBRARIES += questui
-LOCAL_STATIC_LIBRARIES += codegen_0_10_2
-LOCAL_STATIC_LIBRARIES += custom-types
 include $(BUILD_SHARED_LIBRARY)
 
 # In order to make this mod work with BMBF, you must provide a zip file with the specific libbeatsaber-hook.so (file copied to the libs directory)
