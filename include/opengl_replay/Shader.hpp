c104633acc58113f1a01f98bad741146c452c0e5
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-08-05 16:25:01 -0400

    Initial Shader class

---
+++ include/opengl_replay/Shader.hpp
@@ -0,0 +1,28 @@
+// Stolen from https://learnopengl.com/Getting-started/Shaders
+
+#pragma once
+
+#include <glad/glad.h> // include glad to get all the required OpenGL headers
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+
+
+class Shader
+{
+public:
+    // the program ID
+    unsigned int ID;
+
+    // constructor reads and builds the shader
+    Shader(const char* vertexPath, const char* fragmentPath);
+    // use/activate the shader
+    void use();
+    // utility uniform functions
+    void setBool(const std::string &name, bool value) const;
+    void setInt(const std::string &name, int value) const;
+    void setFloat(const std::string &name, float value) const;
+};
+
