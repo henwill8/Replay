2d6ec7fdd6ade3cdc938270c7da00882c7e5ca9a
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2021-08-06 11:49:43 -0400

    Initial shader generation script. Might change to use Shader class too later

---
+++ compile_shaders.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+import zipfile
+
+shader_folder = "./shaders"
+
+shader_header_folder = "./include/shaders"
+
+if os.path.exists(shader_header_folder):
+    print("Clearing shader header folder!")
+    shutil.rmtree(shader_header_folder)
+
+os.makedirs(shader_header_folder)
+
+def shader_file(shader_name, shader_contents):
+    lines = ""
+
+    for line in shader_contents:
+        line = line.replace("\n", "")
+        lines += f"\"{line}\"\n"
+
+    return f"""
+constexpr const char* {shader_name.replace(".","_")} = {lines};
+"""
+
+
+for shader_name in os.listdir(shader_folder):
+    file_path = os.path.join(shader_folder, shader_name)
+    print(f"Making header {shader_name}.hpp in {shader_header_folder}")
+
+    with open(f"{file_path}", "r") as original_shader_file:
+        with open(f"{shader_header_folder}/{shader_name}.hpp", "w") as file_converted:
+            shader_header_code = shader_file(shader_name, original_shader_file.readlines())
+            file_converted.write(shader_header_code)
+
+print("Done making shaders!")
