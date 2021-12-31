243b986fa7ff1ee3134af115f457865c665941ee
Henwill8 <henwill8@yahoo.com>
2021-12-30 23:18:31 -0700

    Revert "more setup"
    This reverts commit 18aad396aef9bda14d7514510ba9d9c9833c9866.

---
+++ include/UI/ReplayViewController.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "HMUI/ViewController.hpp"
+
+DECLARE_CLASS_CODEGEN(Replay::UI, ReplayViewController, HMUI::ViewController,
+
+    /*named instance (no specific impl)*/
+    
+)
--- src/Hooks/StandardLevelDetailView.cpp
+++ src/Hooks/StandardLevelDetailView.cpp
@@ -69,6 +69,56 @@ MAKE_HOOK_MATCH(StandardLevelDetailView_RefreshContent, &StandardLevelDetailView
     if(replayButtonTransform) {
         replayButtonGameObject = replayButtonTransform->get_gameObject();
     } else {
+        // View controller creation here: https://github.com/RedBrumbler/SongBrowser/blob/master/src/UI/Browser/SongBrowserUI.cpp#L146
+        GlobalNamespace::LevelSelectionFlowCoordinator* flowCoordinator = nullptr;
+        switch(mode)
+        {
+            case GlobalNamespace::MainMenuViewController::MenuButton::SoloFreePlay:
+                flowCoordinator = ArrayUtil::Last(Resources::FindObjectsOfTypeAll<GlobalNamespace::SoloFreePlayFlowCoordinator*>());
+                break;
+            case GlobalNamespace::MainMenuViewController::MenuButton::Party:
+                flowCoordinator = ArrayUtil::Last(Resources::FindObjectsOfTypeAll<GlobalNamespace::PartyFreePlayFlowCoordinator*>());
+                break;
+        }
+
+        beatUi = *il2cpp_utils::New<SongBrowser::DataAccess::BeatSaberUIController*>(flowCoordinator);
+        lastLevelCollection = nullptr;
+        
+        auto screenContainer = ArrayUtil::First(Resources::FindObjectsOfTypeAll<Transform*>(), [](auto x) {
+                static Il2CppString* screenContainerName = il2cpp_utils::newcsstr<il2cpp_utils::CreationType::Manual>("ScreenContainer");
+                return screenContainerName->Equals(x->get_name());
+            });
+
+        INFO("screenContainer: %p", screenContainer);
+        
+        auto curvedCanvasSettings = screenContainer->GetComponent<HMUI::CurvedCanvasSettings*>();
+        INFO("curvedCanvasSettings: %p", curvedCanvasSettings);
+
+        INFO("viewController: %p", viewController);
+        if (uiCreated)
+        {
+            auto vcCanvasSettings = viewController->GetComponent<HMUI::CurvedCanvasSettings*>();
+            INFO("vcCanvasSettings: %p", vcCanvasSettings);
+            vcCanvasSettings->SetRadius(curvedCanvasSettings->get_radius());
+            return;
+        }
+
+        if (viewController)
+        {
+            Object::Destroy(viewController);
+        }
+
+        INFO("creating view controller");
+        viewController = UIUtils::CreateCurvedViewController<SongBrowser::UI::SongBrowserViewController*>("SongBrowserViewController", curvedCanvasSettings->get_radius());
+        INFO("viewController: %p", viewController);
+        auto rectTransform = viewController->get_rectTransform();
+        rectTransform->SetParent(beatUi->LevelCollectionNavigationController->get_transform(), false);
+        rectTransform->set_anchorMin(Vector2(0.0f, 0.0f));
+        rectTransform->set_anchorMax(Vector2(1.0f, 1.0f));
+        rectTransform->set_anchoredPosition(Vector2(0.0f, 0.0f));
+        rectTransform->set_sizeDelta(Vector2(curvedCanvasSettings->get_radius(), 25));
+        viewController->get_gameObject()->SetActive(true);
+
         replayButtonGameObject = Object::Instantiate(templateButton->get_gameObject(), parent);
         replayButtonTransform = replayButtonGameObject->get_transform();
         replayButtonGameObject->set_name(replayButtonName);
