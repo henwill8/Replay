38b26286196a811b6c171cee085e3e31a492cb5f
FernTheDev <15272073+Fernthedev@users.noreply.github.com>
2022-01-02 21:34:34 -0400

    optional pog

--- src/Hooks/StandardLevelScenesTransitionSetupDataSO.cpp
+++ src/Hooks/StandardLevelScenesTransitionSetupDataSO.cpp
@@ -18,6 +18,7 @@ using namespace Replay::FileUtils;
 MAKE_HOOK_MATCH(StandardLevelScenesTransitionSetupDataSO_Init, &StandardLevelScenesTransitionSetupDataSO::Init, void, StandardLevelScenesTransitionSetupDataSO* self, Il2CppString* gameMode, GlobalNamespace::IDifficultyBeatmap* difficultyBeatmap, GlobalNamespace::IPreviewBeatmapLevel* previewBeatmapLevel, GlobalNamespace::OverrideEnvironmentSettings* overrideEnvironmentSettings, GlobalNamespace::ColorScheme* overrideColorScheme, GlobalNamespace::GameplayModifiers* gameplayModifiers, GlobalNamespace::PlayerSpecificSettings* playerSpecificSettings, GlobalNamespace::PracticeSettings* practiceSettings, Il2CppString* backButtonText, bool useTestNoteCutSoundEffects) {
     if(ReplayManager::temporaryState == ReplayState::REPLAYING) {
         std::vector<std::string> modifierStrings;
+        log("Doc exists: %s", FileUtils::lastSelectedMetadata.has_value() ? "true" : "false");
         auto const& metadata = FileUtils::lastSelectedMetadata.value();
 
         for(const auto& value : metadata["Modifiers"].GetArray()) {
