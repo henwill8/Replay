4136ce52ba84450ce53bbeedfc79af7f2d9cac0b
Henwill8 <henwill8@yahoo.com>
2022-01-07 15:38:50 -0700

    separate diff and characteristic from hash

--- src/Utils/SongUtils.cpp
+++ src/Utils/SongUtils.cpp
@@ -5,32 +5,28 @@ std::string Replay::SongUtils::GetMapID() {
 }
 
 void Replay::SongUtils::SetMapID(GlobalNamespace::StandardLevelDetailView* standardLevelDetailView) {
-    log("Getting map ID");
-
     auto* Level = reinterpret_cast<GlobalNamespace::BeatmapLevelSO*>(standardLevelDetailView->level);
     if(Level == nullptr) {
         log("Beatmap Level is null");
         return;
     }
-    Il2CppString* LevelID = Level->get_levelID();
-    log("Got map hash");
+    std::string LevelID = to_utf8(csstrtostr(Level->get_levelID()));
+
+    if(LevelID.find("custom_level_") != std::string::npos) {
+        LevelID.erase(LevelID.begin(), LevelID.begin()+13);
+        transform(LevelID.begin(), LevelID.end(), LevelID.begin(), ::tolower);
+    }
 
     auto* SelectedBeatmapDifficulty = standardLevelDetailView->selectedDifficultyBeatmap;
-    
     int Difficulty = SelectedBeatmapDifficulty->get_difficulty();
-    log("Got map difficulty");
 
     auto* beatMapData = SelectedBeatmapDifficulty->get_beatmapData();
     auto* parentDifficultyBeatmapSet = SelectedBeatmapDifficulty->get_parentDifficultyBeatmapSet();
     auto* beatmapCharacteristic = parentDifficultyBeatmapSet->get_beatmapCharacteristic();
     std::string modeName = to_utf8(csstrtostr(beatmapCharacteristic->compoundIdPartName));
-    log("Got map mode");
 
-    mapID = to_utf8(csstrtostr(LevelID)) + std::to_string(Difficulty) + modeName;
-    if(mapID.find("custom_level_") != std::string::npos) {
-        log("Removing 'custom_level_' from hash");
-        mapID.erase(mapID.begin(), mapID.begin()+13);
-        transform(mapID.begin(), mapID.end(), mapID.begin(), ::tolower);
-    }
+    mapID = LevelID + "_" + std::to_string(Difficulty);
+    if(!modeName.empty()) mapID = mapID + "_" + modeName;
+
     log("MapID is %s", mapID.c_str());
 }
